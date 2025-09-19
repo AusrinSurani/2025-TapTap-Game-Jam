a30ac51aae12958452a558aa4d9ffc9afd62aed8
oneachai <1519374711@qq.com>
2025-09-19 20:20:08 +0800

    push test

---
+++ Tap Tap Game Jam/Assets/Scripts/TestUse.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUse : MonoBehaviour
+{
+
+    //for test use 测试编码格式
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
